5d51134d176526a6ef151f9c46ffd07f9ba0f869
stilyanpetrov <f.and.g.2019@gmail.com>
2023-09-24 07:21:23 +0300

    replace all the headers in html files with fragment header.

--- mobilele/src/main/java/org/softuni/mobilele/web/HomeController.java
+++ mobilele/src/main/java/org/softuni/mobilele/web/HomeController.java
@@ -1,5 +1,7 @@
 package org.softuni.mobilele.web;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
